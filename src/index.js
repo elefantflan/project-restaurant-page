1bc775299ff62da872d23796ae3d8d0d52612d82
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-22 09:57:31 -0700

    put asset loader within config under output>assetModuleFilename:'[name][ext]' and module>rules>asset/resource

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('./styles/main.scss');
 
 import getPage from "./menu.js";
+import restaurantImg from './assets/beachhouse.jpg';
 
 const menu = document.getElementById('menu');
 menu.addEventListener('click', ()=>{
