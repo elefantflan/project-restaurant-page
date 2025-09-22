f30533a378c59dabed6e46836e08a2f7175dda66
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-22 13:11:13 -0700

    fixed caption

--- src/home.js
+++ src/home.js
@@ -16,7 +16,7 @@ function getPageHome(){
     div.append(info);
 
     title.textContent="Your finest seafood dining experience";
-    info.textContent="Innisfree lets you breathe, slow down, and relax.\nWith our view by the beautiful sea and meals cookedfrom fresh, local sources,\nwe guarantee you won't want to look anywhere else to taste the best of the ocean.";
+    info.textContent="Innisfree lets you breathe, slow down, and relax. With our view by the beautiful sea and meals cooked from fresh, local sources, we guarantee you won't want to look anywhere else to taste the best of the ocean.";
 
     return div;
 }
